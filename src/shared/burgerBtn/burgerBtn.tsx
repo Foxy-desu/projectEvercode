ae973c6de0e9c83c7516b616524be00ef2b29884
Alexandra Orliuk <iammeduza@gmail.com>
2024-09-24 22:11:10 +0300

    feat: add burger btn

---
+++ src/shared/burgerBtn/burgerBtn.tsx
@@ -0,0 +1,22 @@
+import cl from './burgerBtn.module.scss';
+
+interface IBurgerBtnProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  isBurgerOpen: boolean;
+  colorScheme?: "light" | "dark";
+}
+const BurgerBtn =({colorScheme='dark', isBurgerOpen,...props}:IBurgerBtnProps)=>{
+  const btnStyle = `${cl.burgerBtn} ${isBurgerOpen? cl.burgerBtn_opened: cl.burgerBtn_closed}`;
+  const lineStyle = `${cl.line} ${colorScheme === "light"? cl.line_light : cl.line_dark}`
+  return (
+    <button className={btnStyle} {...props}>
+      <span className='visually-hidden'>
+        {isBurgerOpen? 'Закрыть меню':'Открыть меню'}
+      </span>
+        <span className={lineStyle}></span>
+        <span className={lineStyle}></span>
+        <span className={lineStyle}></span>
+    </button>
+  )
+};
+
+export default BurgerBtn;
