54c649c2d8fffa36578c9be58fd3c9bbdee97791
Alexandra Orliuk <iammeduza@gmail.com>
2024-09-26 21:54:51 +0300

    feat: create section component

---
+++ src/shared/section/section.tsx
@@ -0,0 +1,20 @@
+import { forwardRef } from 'react';
+import { parseHTML } from '../utils/helpers';
+import cl from './section.module.scss';
+
+interface ISection {
+  sectionTitle: string;
+  children: React.ReactNode;
+};
+export type TRef = HTMLHeadingElement;
+
+const Section = forwardRef<TRef, ISection>(({children, sectionTitle},ref)=> {
+  return (
+    <section className={cl.section}>
+      <h2 className={cl.title} ref={ref}>{parseHTML(sectionTitle)}</h2>
+      {children}
+    </section>
+  )
+});
+
+export default Section;
