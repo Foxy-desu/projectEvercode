11236fb88152f0aed9fca8f815655e773b3ded8a
Alexandra Orliuk <iammeduza@gmail.com>
2024-09-28 00:31:44 +0300

    feat: add app list

---
+++ src/app/appData/appList.ts
@@ -0,0 +1,23 @@
+// import cadfem from '../assets/img/png/cadfem.png';
+// import foodfox from '../assets/img/png/foodfox.png';
+// import tdcloud from '../assets/img/png/td-cloud.png';
+// import reksoft from '../assets/img/png/reksoft.png';
+// import guardaWallet from '../assets/img/png/guarda.png';
+// import hellan from '../assets/img/png/gelan.png';
+// import frostWallet from '../assets/img/png/frostWallet.png';
+// import ondock from '../assets/img/png/ondock.png';
+// import kelnik from '../assets/img/png/kelnik.png';
+// import cherrySwap from '../assets/img/png/cherry.png';
+
+export const appList = [
+  { name: 'Cadfem', logo: "cadfem", url: 'https://www.cadfem-cis.ru/' },
+  { name: 'Foodfox<br/>(now Yandex.Eats)', logo: "foodfox", url: 'https://evercodelab.com/ru/cases/foodfox/' },
+  { name: 'TDCloud', logo: "td-cloud", url: 'https://tdcloud.ru/' },
+  { name: 'Reksoft', logo: "reksoft", url: 'https://www.reksoft.ru/' },
+  { name: 'Guarda Wallet', logo: "guarda", url: 'https://guarda.com/' },
+  { name: 'Hellan', logo: "gelan", url: 'https://evercodelab.com/ru/cases/escar/' },
+  { name: 'Frost Wallet', logo: "frostwallet", url: 'https://frostwallet.cc/' },
+  { name: 'Ondock', logo: "ondoc", url: 'https://ondoc.me/'},
+  { name: 'Kelnik', logo: "kelnik", url: 'https://www.kelnik.ru/' },
+  { name: 'Cherry Swap', logo: "cherry", url: 'https://www.cherryswap.io/home/' },
+]
