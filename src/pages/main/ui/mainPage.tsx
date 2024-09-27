222d15b1ede4316650f4c7bef4e0fa71959197fa
Alexandra Orliuk <iammeduza@gmail.com>
2024-09-28 00:37:29 +0300

    feat: add flexbox with cards

--- src/pages/main/ui/mainPage.tsx
+++ src/pages/main/ui/mainPage.tsx
@@ -5,8 +5,22 @@ import cl from './mainPage.module.scss';
 import { TRef } from '../../../shared/section/section';
 import { useRef } from 'react';
 import Section from '../../../shared/section/section';
-
+import { appList } from '../../../app/appData/appList';
+import CardWithLogo from '../../../shared/cardWithLogo/cardWithLogo';
+import { useViewportWidth } from '../../../shared/utils/hooks/getViewportWidth';
+import { useStateOnThreshold } from '../../../shared/utils/hooks/useStateOnThreshold';
+import RoundBtn from '../../../shared/roundBtn/roundBtn';
+ // TODO: implement sliders, slider cards and footer
+ // TODO: improve styling for different viewportwidth
+ // TODO: fix viewportwidth getting (for different browsers viewports)
+ // TODO: implement good 404 page
+ // TODO: implement lazyLoading of pages
 const MainPage =()=>{
+  const threshold = 704;
+  const viewportWidth = useViewportWidth();
+  const {isThresholdReached:shouldBlockChange, resetThresholdReach} = useStateOnThreshold({value:viewportWidth, compareOperator:'<', threshold, reconcilation: false});
+
+
   const scrollTarget = useRef<TRef>(null)
   const heroImages:IHeroSectionProps['sectionImages'] = {
     big: {
@@ -18,7 +32,6 @@ const MainPage =()=>{
       width: 395,
     },
   };
-
   function slideIntoView(){
     const scrollTargetNode = scrollTarget.current;
     if(scrollTargetNode) {
@@ -27,7 +40,7 @@ const MainPage =()=>{
         block:'start',
       });
     }
-  }
+  };
   return (
     <>
     <HeroSection
@@ -40,7 +53,33 @@ const MainPage =()=>{
       <Section
         ref={scrollTarget}
         sectionTitle='<span>Разрабатываем микросервисы,</span> мобильные и&nbsp;веб-приложения для бизнеса и&nbsp;стартапов'>
-        <p>oh blyat'</p>
+        <ul className={cl.appList}>
+          {appList.map((item) => {
+            const listItemStyle = `${cl.listItem} ${shouldBlockChange? cl.reduced: ''}`
+            return (
+              <li className={listItemStyle} key={item.name} >
+                <CardWithLogo logo={item.logo} title={item.name} link={item.url}/>
+              </li>
+            )
+          })}
+        </ul>
+        {shouldBlockChange
+          ?(
+            <div className={cl.btnWrap}>
+              <RoundBtn purpose='expandControl' prompt='Раскрыть список' onClick={resetThresholdReach} />
+            </div>
+          )
+        : ''}
+      </Section>
+      <Section sectionTitle='Наши продукты'>
+        <ul className={cl.serviceList}>
+          {/* Add your service list items here */}
+        </ul>
+      </Section>
+      <Section sectionTitle='Наша экспертиза'>
+        <ul className={cl.serviceList}>
+          {/* Add your service list items here */}
+        </ul>
       </Section>
     </div>
     </>
