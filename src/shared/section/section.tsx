8739902ed447acd4dc42f1ae6a3a1aa5728e3bfa
Alexandra Orliuk <iammeduza@gmail.com>
2024-10-02 14:08:38 +0300

    fix: make section component type extended from div to receive class

--- src/shared/section/section.tsx
+++ src/shared/section/section.tsx
@@ -1,16 +1,16 @@
-import { forwardRef } from 'react';
+import { HTMLAttributes, forwardRef } from 'react';
 import { parseHTML } from '../utils/helpers';
 import cl from './section.module.scss';
 
-interface ISection {
+interface ISection extends HTMLAttributes<HTMLDivElement> {
   sectionTitle?: string;
   children: React.ReactNode;
 };
 export type TRef = HTMLHeadingElement;
 
-const Section = forwardRef<TRef, ISection>(({children, sectionTitle},ref)=> {
+const Section = forwardRef<TRef, ISection>(({children, sectionTitle, className,...rest},ref)=> {
   return (
-    <section className={cl.section}>
+    <section className={`${cl.section} ${className && className}`} {...rest}>
       {sectionTitle && <h2 className={cl.title} ref={ref}>{parseHTML(sectionTitle)}</h2>}
       {children}
     </section>
