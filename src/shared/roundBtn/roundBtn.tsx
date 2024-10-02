e764d2a7c35c12ac99c44355ebc3255e5edfc101
Alexandra Orliuk <iammeduza@gmail.com>
2024-10-02 23:32:31 +0300

    feat: add forward ref to button

--- src/shared/roundBtn/roundBtn.tsx
+++ src/shared/roundBtn/roundBtn.tsx
@@ -1,16 +1,17 @@
+import { forwardRef } from 'react';
 import cl from './roundBtn.module.scss';
 interface IRoundBtn extends React.HTMLAttributes<HTMLButtonElement> {
   prompt: string;
   purpose?: 'slideControl' | 'expandControl'
 }
 
-const RoundBtn =({purpose='slideControl',prompt,...props}:IRoundBtn)=> {
+const RoundBtn =forwardRef<HTMLButtonElement, IRoundBtn>(({purpose='slideControl',prompt,...props}:IRoundBtn, ref)=> {
   const btnStyle = `${cl.btn} ${purpose === 'slideControl' ? cl.btn_slide : cl.btn_expand}`
   return (
-    <button className={btnStyle} type='button' {...props}>
+    <button className={btnStyle} type='button' {...props} ref={ref}>
       <span className='visually-hidden'>{prompt}</span>
     </button>
   )
-};
+});
 
 export default RoundBtn;
