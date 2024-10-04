132d068a0f1804d88d2ab225bb8c50c9cfb72c8b
Alexandra Orliuk <iammeduza@gmail.com>
2024-10-05 01:52:29 +0300

    feat: create social link component

---
+++ src/shared/socialLink/socialLink.tsx
@@ -0,0 +1,22 @@
+import { HTMLAttributes } from 'react';
+import cl from './socialLink.module.scss';
+
+interface ISocialLink extends HTMLAttributes<HTMLAnchorElement> {
+  srText: string;
+  iconSource: string;
+  href: string;
+}
+const SocialLink =({srText,iconSource,...rest}: ISocialLink)=> {
+  return (
+    <a className={cl.link} {...rest}>
+      <span className="visually-hidden">
+        {srText}
+      </span>
+      <svg className={cl.icon}>
+        <use xlinkHref={iconSource} />
+      </svg>
+    </a>
+  )
+};
+
+export default SocialLink; 
