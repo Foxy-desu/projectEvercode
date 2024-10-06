6427d4be8da41c79a4c46a8cadbc35492e2d3951
Alexandra Orliuk <iammeduza@gmail.com>
2024-10-07 01:24:59 +0300

    feat: implement loader screen along with cookie banner inside root layout

--- src/app/routs/root.tsx
+++ src/app/routs/root.tsx
@@ -1,16 +1,39 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import { navMenuLinks } from '../appData/navigationData';
 import Header from '../../widgets/header/header.tsx';
 import Footer from '../../widgets/footer/footer.tsx';
+import CookieBannerContainer from '../../features/cookieBanner/model/cookieBannerContainer.tsx';
+import cl from './root.module.scss';
 
 const RootElement =()=> {
+  const {state} = useNavigation();
   return (
     <>
       <Header navMenuLinks={navMenuLinks}/>
-      <main>
+      <main className={cl.main}>
+        {
+          state === 'loading' && (
+            <div 
+              style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                background: '#fff',
+                opacity: 0.7
+                }}>
+              Загрузка страницы. Пожалуйста, подождите...
+            </div>
+          ) 
+        }  
         <Outlet/>
       </main>
       <Footer navMenuLinks={navMenuLinks}/>
+      <CookieBannerContainer/>
     </>
   )
 };
