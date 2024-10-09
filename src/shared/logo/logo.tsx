5ab1fb058159b5ae8d29e66928df30ed8d3bbbcf
Alexandra Orliuk <iammeduza@gmail.com>
2024-10-09 05:07:35 +0300

    refactor: edit logo component

--- src/shared/logo/logo.tsx
+++ src/shared/logo/logo.tsx
@@ -2,15 +2,12 @@ import light from '../../app/assets/img/svg/logo-light.svg';
 import dark from '../../app/assets/img/svg/logo_dark.svg';
 import cl from './logo.module.scss';
 
-interface IMainLogoProps {
+interface IMainLogo {
   scheme: "light" | "dark";
-  alt: string;  // alt text for screen readers or SEO purpose
+  alt: string;
 }
-const MainLogo =({scheme, alt}:IMainLogoProps)=>{
-
-  return (
-    <img className={cl.logo} src={scheme === "light"? light:dark} alt={alt} loading='lazy'/>
-  )
+const MainLogo =({scheme, alt}:IMainLogo)=>{
+  return <img className={cl.logo} src={scheme === "light"? light:dark} alt={alt}/>
 };
 
 export default MainLogo;
