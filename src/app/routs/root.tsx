06b4318910a69b33c4eacbf4219c8af36293128a
Alexandra Orliuk <iammeduza@gmail.com>
2024-10-09 05:08:20 +0300

    refactor: implement loader component

--- src/app/routs/root.tsx
+++ src/app/routs/root.tsx
@@ -1,41 +1,26 @@
 import { Outlet, useNavigation } from 'react-router-dom';
 import { navMenuLinks } from '../appData/navigationData';
-import loader from '../assets/img/svg/loader.svg';
-import Header from '../../widgets/header/header.tsx';
+import { useViewportWidth } from '../../shared/utils/hooks/getViewportWidth.ts';
+import { WidthContext } from '../context/widthContext.ts';
 import Footer from '../../widgets/footer/footer.tsx';
 import CookieBannerContainer from '../../features/cookieBanner/model/cookieBannerContainer.tsx';
+import Loader from '../../shared/loader/loader.tsx';
+import HeaderContainer from '../../widgets/header/model/headerContainer.tsx';
 import cl from './root.module.scss';
 
 const RootElement =()=> {
+  const viewportWidthData = useViewportWidth();
   const {state} = useNavigation();
   return (
-    <>
-      <Header navMenuLinks={navMenuLinks}/>
+    <WidthContext.Provider value={viewportWidthData}>
+      <HeaderContainer navMenuLinks={navMenuLinks}/>
       <main className={cl.main}>
-        {
-          state === 'loading' && (
-            <div 
-              style={{
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                background: '#fff',
-                opacity: 0.7
-                }}>
-              <img src={loader} alt="Загрузка страницы"/>
-            </div>
-          ) 
-        }  
+        {state === 'loading' && <Loader/>}  
         <Outlet/>
       </main>
       <Footer navMenuLinks={navMenuLinks}/>
       <CookieBannerContainer/>
-    </>
+    </WidthContext.Provider>
   )
 };
 
